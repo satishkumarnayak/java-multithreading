38aa036840eeba0ba6ba1aed7edea635f9eee408
Satish Nayak <saishkumarNayak@gmail.com>
2019-06-28 14:35:50 +0530

    Sequential processing

---
+++ multithreading/src/main/java/com/instanceofcake/multithreading/Sequential.java
@@ -0,0 +1,32 @@
+package com.instanceofcake.multithreading;
+
+public class Sequential {
+
+	public static void main(String[] args) {
+		Runner1 runner1 = new Runner1();
+		Runner2 runner2 = new Runner2();
+
+		runner1.startRunning();
+		runner2.startRunning();
+
+	}
+
+}
+
+class Runner1 {
+
+	public void startRunning() {
+		for (int i = 1; i <= 5; i++) {
+			System.out.println("Runner1->" + i);
+		}
+	}
+}
+
+class Runner2 {
+
+	public void startRunning() {
+		for (int i = 1; i <= 5; i++) {
+			System.out.println("Runner2->" + i);
+		}
+	}
+}
