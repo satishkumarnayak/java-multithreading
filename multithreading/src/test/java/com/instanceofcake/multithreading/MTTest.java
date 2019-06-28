e199e6e5e178ec22cf689427fbab4c51d6da7b18
Satish Nayak <saishkumarNayak@gmail.com>
2019-06-28 14:29:26 +0530

    Initial project setup

---
+++ multithreading/src/test/java/com/instanceofcake/multithreading/MTTest.java
@@ -0,0 +1,19 @@
+package com.instanceofcake.multithreading;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class MTTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
